7c09113ade96cf138430da00715049e35a0b5e16
pastushak <pastushak_roman@kolgym.if.ua>
2025-04-23 11:27:56 +0300

    Update file

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://[jhuwkklgszqiupzqithf].supabase.co'
-const supabaseKey = '[eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpodXdra2xnc3pxaXVwenFpdGhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzODY1NTEsImV4cCI6MjA2MDk2MjU1MX0.EWPxl6sJ2f-UI66bx-nizUutwdDA7KTMLoPb30gVLNk]'
+const supabaseUrl = 'https://jhuwkklgszqiupzqithf.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpodXdra2xnc3pxaXVwenFpdGhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzODY1NTEsImV4cCI6MjA2MDk2MjU1MX0.EWPxl6sJ2f-UI66bx-nizUutwdDA7KTMLoPb30gVLNk'
 
 // Ці значення можна знайти в розділі Project Settings > API
 export const supabase = createClient(supabaseUrl, supabaseKey)
