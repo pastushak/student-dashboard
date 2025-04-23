6af62afe005a6c989e83a56b9c9962dc4ac30138
pastushak <pastushak_roman@kolgym.if.ua>
2025-04-23 11:19:37 +0300

    Updates

---
+++ static/js/952.82f1f273.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstudent_dashboard=self.webpackChunkstudent_dashboard||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.82f1f273.chunk.js.map
